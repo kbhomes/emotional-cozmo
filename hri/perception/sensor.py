2f003d8e88ad94b2ec440f90a4a5e64df72fb487
Sajid Anwar <sajidanwar94@gmail.com>
2016-11-19 14:49:21 -0500

    Test vision sensor: simulate toy stimulus

--- hri/perception/sensor.py
+++ hri/perception/sensor.py
@@ -10,26 +10,42 @@ class Vision(object):
         self.perception_system = perception_system
 
         # For testing!
-        self.test_target = random.randint(-100, 100)
-        self.test_current = 0
+        self.test_face_target = random.randint(-100, 100)
+        self.test_face_current = 0
+        self.test_toy_target = random.randint(-100, 100)
+        self.test_toy_current = 0
 
     def update(self, elapsed):
         self.test_update(elapsed)
 
     def test_update(self, elapsed):
         """ Generate random detections and disappearances for a stimulus """
-        if self.test_current == self.test_target:
-            self.test_target = random.randint(-100, 100)
+        if self.test_face_current == self.test_face_target:
+            self.test_face_target = random.randint(-100, 100)
 
-        delta = min(math.floor(random.randint(0, 40) * elapsed), abs(self.test_target - self.test_current))
+        if self.test_toy_current == self.test_toy_target:
+            self.test_toy_target = random.randint(-100, 100)
+
+        delta_face = min(math.floor(random.randint(0, 40) * elapsed), abs(self.test_face_target - self.test_face_current))
+        delta_toy = min(math.floor(random.randint(0, 40) * elapsed), abs(self.test_toy_target - self.test_toy_current))
         
-        if self.test_current < self.test_target:
-            self.test_current += delta
+        if self.test_face_current < self.test_face_target:
+            self.test_face_current += delta_face
         else:
-            self.test_current -= delta
+            self.test_face_current -= delta_face
+        
+        if self.test_toy_current < self.test_toy_target:
+            self.test_toy_current += delta_toy
+        else:
+            self.test_toy_current -= delta_toy
 
         # Generate events based upon this smooth randomness
-        if self.test_current > 0:
+        if self.test_face_current > 0:
             self.perception_system.stimuli['face-1'].detect()
         else:
             self.perception_system.stimuli['face-1'].disappear()
+
+        if self.test_toy_current > 0:
+            self.perception_system.stimuli['toy-1'].detect()
+        else:
+            self.perception_system.stimuli['toy-1'].disappear()
